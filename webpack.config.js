e8befc1ebf88ef4de57185ae11cb204e6a67dcc4
Irvin Contreras <irvincnt93@gmail.com>
2017-12-06 19:27:34 -0600

    configuración de webpack para correr un hola mundo

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const webpack = require('webpack'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
+    cssModules = 'modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]',
+    path = require("path");
+
+module.exports = {
+    devtool:'scheap-eval-source-map',
+    resolve: {
+        extensions: [".js",".jsx",".css"]
+
+    },
+    context: path.resolve(__dirname, 'src'),
+    entry: './index.jsx',
+    //entry: ['./src/index.jsx'], //entrada
+    output: { //salida
+        filename: 'app.js', //nombre del archivo
+        path: path.resolve(__dirname, "build"), //donde se situara el fichero de salida
+    },
+    module: {
+        loaders: [{
+                test: /(\.js|jsx)$/,
+                exclude: path.resolve(__dirname + '/node_modules/'),
+                use: [{
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['react', 'latest']
+                    }
+                }]
+            },
+            {
+                test: /\.css$/,
+                loader: `style-loader!css-loader`
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "build"),
+        compress: true,
+        port: 8080,
+        open: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: `index.html`,
+            template: `assets/index.html`,
+        }),
+        new ExtractTextPlugin({
+            filename: 'style.css',
+            allChunks: true
+        })
+    ]
+};
