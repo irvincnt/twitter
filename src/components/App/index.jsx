71873aaeba181e4d1e99566c90f90abcd3bcf800
Irvin Contreras <irvincnt93@gmail.com>
2017-12-08 07:02:58 -0600

    Registrar datos del usuarios en el componenten padre (app) y pasar el state de componente app a main

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -7,11 +7,25 @@ import  Main from '../Main'
 
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      user: {
+        text: 'mensaje de prueba',
+        photoURL: 'https://pbs.twimg.com/profile_images/827790443152805888/hy08L7YR_bigger.jpg',
+        displayName: 'Irvin Contreras',
+        email: 'irvincnt@gmail.com',
+        date: Date.now() - 1800000,
+        onOpenText: false
+      }
+    }
+  }
+
   render() {
     return(
       <div>
         <Header/>
-        <Main />
+        <Main user={this.state.user}/>
       </div>
     )
   }
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -12,21 +12,7 @@ class Main extends Component {
           picture: 'https://pbs.twimg.com/profile_images/827790443152805888/hy08L7YR_bigger.jpg',
           displayName: 'Irvin Contreras',
           username: 'irvincnt',
-          date: Date.now() - 180000
-        },
-        {
-          text: 'mensaje de prueba',
-          picture: 'https://pbs.twimg.com/profile_images/827790443152805888/hy08L7YR_bigger.jpg',
-          displayName: 'Irvin Contreras',
-          username: 'irvincnt',
-          date: Date.now() - 180000
-        },
-        {
-          text: 'mensaje de prueba',
-          picture: 'https://pbs.twimg.com/profile_images/827790443152805888/hy08L7YR_bigger.jpg',
-          displayName: 'Irvin Contreras',
-          username: 'irvincnt',
-          date: Date.now() - 180000
+          date: Date.now() - 1800000
         },
         {
           text: 'mensaje de prueba',
@@ -41,8 +27,14 @@ class Main extends Component {
 
   render() {
     return(
-    <MessageList messages={this.state.messages}/>
-    // <div>holaaaaaa</div>
+      <div>
+        <ProfileBar
+          picture={this.props.user.photoURL}
+          username={this.props.user.email.split('@')[0]}
+          onOpenText={this.handlerOpenText}
+        />
+        <MessageList messages={this.state.messages}/>
+      </div>
     )
   }
 }
